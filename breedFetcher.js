e37a1f6da23736e1860e0c3a65931a239ab0accf
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-15 11:46:58 -0330

    1stV

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,21 @@
 const request = require('request');
 
+const id = process.argv.slice(2);
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${id}`, (error, response, body) => {
+  if (error) {
+    console.error("Request error:", error);
+    return;
+  }
+  const data = JSON.parse(body);
+  if (data.length > 0) {
+    console.log(data[0].description);
+  } else {
+    console.log("Breed not found");
+  }
+});
+
+
+
+
